e580743413a578f9ce8c5d08adeedb261a9c2446
Zhang Maiyun <myzhang1029@hotmail.com>
2021-08-30 21:58:52 -0700

    Initialize

---
+++ HackSteps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackSteps
+//
+//  Created by 张迈允 on 2021/8/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackSteps/HackStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackStepsApp.swift
+//  HackSteps
+//
+//  Created by 张迈允 on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct HackStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
